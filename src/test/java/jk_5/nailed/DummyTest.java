cd2a2ee3a89c9e8bcabe528a739c189c57639591
jk-5 <jeffreykog@me.com>
2013-12-04 16:35:54 +0100

    Added a dummy test for jenkins configuration

---
+++ src/test/java/jk_5/nailed/DummyTest.java
@@ -0,0 +1,17 @@
+package jk_5.nailed;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * No description given
+ *
+ * @author jk-5
+ */
+public class DummyTest {
+
+    @Test
+    public void test(){
+        Assert.assertTrue(true);
+    }
+}
