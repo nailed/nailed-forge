f834496b3cb5d9a4330614a593c7ddc205c9a4a1
jk-5 <jeffreykog@me.com>
2014-01-17 09:12:38 +0100

    Added missing changes

--- Server/src/main/java/jk_5/nailed/network/NailedConnectionHandler.java
+++ Server/src/main/java/jk_5/nailed/network/NailedConnectionHandler.java
@@ -5,12 +5,20 @@ import cpw.mods.fml.common.gameevent.PlayerEvent;
 import cpw.mods.fml.common.network.FMLOutboundHandler;
 import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.relauncher.Side;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufOutputStream;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.embedded.EmbeddedChannel;
 import jk_5.nailed.NailedServer;
 import jk_5.nailed.map.Map;
 import jk_5.nailed.map.MapLoader;
 import jk_5.nailed.map.gen.NailedWorldProvider;
 import net.minecraftforge.common.network.ForgeMessage;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * No description given
@@ -33,6 +41,15 @@ public class NailedConnectionHandler {
             channel.attr(FMLOutboundHandler.FML_MESSAGETARGET).set(FMLOutboundHandler.OutboundTarget.PLAYER);
             channel.attr(FMLOutboundHandler.FML_MESSAGETARGETARGS).set(event.player);
             channel.writeOutbound(packet);
+        }
+
+        ByteBuf buf = Unpooled.buffer();
+        try{
+            BufferedImage image = ImageIO.read(new File("testskin.png"));
+            ImageIO.write(image, "PNG", new ByteBufOutputStream(buf));
+            NailedNetworkHandler.sendPacketToPlayer(new NailedPacket.StoreSkin("test", false, buf), event.player);
+        }catch(IOException e){
+            buf.release();
         }
     }
 }
