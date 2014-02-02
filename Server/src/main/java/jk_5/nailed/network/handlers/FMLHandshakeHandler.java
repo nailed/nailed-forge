0cde34a46e8fe50b4e33a0e3a69e4699917296c8
jk-5 <jeffreykog@me.com>
2014-02-02 21:18:03 +0100

    Finally fixed dimension register packets

--- Server/src/main/java/jk_5/nailed/network/handlers/FMLHandshakeHandler.java
+++ Server/src/main/java/jk_5/nailed/network/handlers/FMLHandshakeHandler.java
@@ -1,6 +1,7 @@
 package jk_5.nailed.network.handlers;
 
 import cpw.mods.fml.common.network.FMLEmbeddedChannel;
+import cpw.mods.fml.common.network.FMLOutboundHandler;
 import cpw.mods.fml.common.network.NetworkHandshakeEstablished;
 import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.relauncher.Side;
@@ -11,7 +12,6 @@ import jk_5.nailed.map.Map;
 import jk_5.nailed.map.MapLoader;
 import jk_5.nailed.network.MinecraftPacketAdapter;
 import net.minecraft.network.NetHandlerPlayServer;
-import net.minecraft.network.Packet;
 import net.minecraftforge.common.network.ForgeMessage;
 
 /**
@@ -24,15 +24,16 @@ public class FMLHandshakeHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception{
         if(evt instanceof NetworkHandshakeEstablished){
-            NetHandlerPlayServer handler = (NetHandlerPlayServer) ((NetworkHandshakeEstablished) evt).netHandler;
+            NetworkHandshakeEstablished event = (NetworkHandshakeEstablished) evt;
+            NetHandlerPlayServer handler = (NetHandlerPlayServer) event.netHandler;
             FMLEmbeddedChannel channel = NetworkRegistry.INSTANCE.getChannel("FORGE", Side.SERVER);
-            //channel.attr(FMLOutboundHandler.FML_MESSAGETARGET).set(FMLOutboundHandler.OutboundTarget.REPLY);
+            channel.attr(FMLOutboundHandler.FML_MESSAGETARGET).set(FMLOutboundHandler.OutboundTarget.DISPATCHER);
+            channel.attr(FMLOutboundHandler.FML_MESSAGETARGETARGS).set(event.dispatcher);
             for(Map map : MapLoader.instance().getMaps()){
                 if(map.getID() >= -1 && map.getID() <= 1){
                     continue;
                 }
-                Packet packet = channel.generatePacketFrom(new ForgeMessage.DimensionRegisterMessage(map.getID(), NailedServer.getProviderID()));
-                handler.func_147359_a(packet);
+                channel.writeOutbound(new ForgeMessage.DimensionRegisterMessage(map.getID(), NailedServer.getProviderID()));
             }
 
             //Try to hack a handler into Minecraft's main pipeline so we can change some packets before they are sent
