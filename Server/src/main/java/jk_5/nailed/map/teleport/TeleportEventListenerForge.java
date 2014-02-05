15c0668029fcb0578fbbfe0fc631c1f42dac8b8a
jk-5 <jeffreykog@me.com>
2014-02-05 14:21:11 +0100

    Don't call FML's DimensionChanged event when teleported to the same map

--- Server/src/main/java/jk_5/nailed/map/teleport/TeleportEventListenerForge.java
+++ Server/src/main/java/jk_5/nailed/map/teleport/TeleportEventListenerForge.java
@@ -13,7 +13,7 @@ public class TeleportEventListenerForge {
 
     @SubscribeEvent
     public void onTeleportEnd(TeleportEvent.TeleportEventEnd event){
-        if((event.entity instanceof EntityPlayer)){
+        if(event.entity instanceof EntityPlayer && event.oldMap != event.newMap){
             FMLCommonHandler.instance().firePlayerChangedDimensionEvent((EntityPlayer) event.entity, event.oldMap.getID(), event.newMap.getID());
         }
     }
