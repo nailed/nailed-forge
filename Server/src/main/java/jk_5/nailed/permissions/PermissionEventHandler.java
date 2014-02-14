44d8118af88ba539c436967058373e670c4cd5d4
jk-5 <jeffreykog@me.com>
2014-02-14 16:01:54 +0100

    Allow permissions to be set for commandblocks

--- Server/src/main/java/jk_5/nailed/permissions/PermissionEventHandler.java
+++ Server/src/main/java/jk_5/nailed/permissions/PermissionEventHandler.java
@@ -2,6 +2,7 @@ package jk_5.nailed.permissions;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import jk_5.nailed.server.command.PermissionCommand;
+import net.minecraft.command.server.CommandBlockLogic;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.util.EnumChatFormatting;
@@ -25,6 +26,14 @@ public class PermissionEventHandler {
             if(event.sender instanceof EntityPlayer){
                 EntityPlayer sender = (EntityPlayer) event.sender;
                 if(!PermissionsManager.checkPerm(sender, node)){
+                    event.setCanceled(true);
+                    ChatComponentTranslation message = new ChatComponentTranslation("commands.generic.permission");
+                    message.getChatStyle().setColor(EnumChatFormatting.RED);
+                    event.sender.addChatMessage(message);
+                }
+            }else if(event.sender instanceof CommandBlockLogic){
+                CommandBlockLogic sender = (CommandBlockLogic) event.sender;
+                if(!PermissionsManager.getPerm("[CommandBlock]", node).check()){
                     event.setCanceled(true);
                     ChatComponentTranslation message = new ChatComponentTranslation("commands.generic.permission");
                     message.getChatStyle().setColor(EnumChatFormatting.RED);
