af005223691dd6ba5084f93d6c7a84663e781614
jk-5 <jeffreykog@me.com>
2014-02-28 14:14:06 +0100

    Cleanup TermApi

--- Server/src/main/java/jk_5/nailed/map/script/api/TermApi.java
+++ Server/src/main/java/jk_5/nailed/map/script/api/TermApi.java
@@ -1,6 +1,9 @@
 package jk_5.nailed.map.script.api;
 
-import jk_5.nailed.map.script.*;
+import jk_5.nailed.map.script.IAPIEnvironment;
+import jk_5.nailed.map.script.ILuaAPI;
+import jk_5.nailed.map.script.ILuaContext;
+import jk_5.nailed.map.script.Terminal;
 
 /**
  * No description given
@@ -10,53 +13,70 @@ import jk_5.nailed.map.script.*;
 public class TermApi implements ILuaAPI {
 
     private Terminal terminal;
-    private ScriptingMachine machine;
 
-    public TermApi(IAPIEnvironment _environment){
-        this.terminal = _environment.getTerminal();
-        this.machine = _environment.getMachine();
+    public TermApi(IAPIEnvironment env){
+        this.terminal = env.getTerminal();
     }
 
+    @Override
     public String[] getNames(){
         return new String[]{"term"};
     }
 
+    @Override
     public void startup(){
+
     }
 
-    public void advance(double _dt){
+    @Override
+    public void advance(double dt){
+
     }
 
+    @Override
     public void shutdown(){
+
     }
 
-    public String[] getMethodNames(){
-        return new String[]{"write", "scroll", "setCursorPos", "setCursorBlink", "getCursorPos", "getSize", "clear", "clearLine", "setTextColour", "setTextColor", "setBackgroundColour", "setBackgroundColor"};
-    }
-
-    public static int parseColour(Object[] args) throws Exception{
-        if((args.length != 1) || (args[0] == null) || (!(args[0] instanceof Double))){
+    public static int parseColor(Object[] args) throws Exception{
+        if(args.length != 1 || args[0] == null || !(args[0] instanceof Double)){
             throw new Exception("Expected number");
         }
         int colour = (int) ((Double) args[0]).doubleValue();
         if(colour <= 0){
-            throw new Exception("Colour out of range");
+            throw new Exception("Color out of range");
         }
         colour = 16 - getHighestBit(colour);
-        if((colour < 0) || (colour > 15)){
-            throw new Exception("Colour out of range");
+        if(colour < 0 || colour > 15){
+            throw new Exception("Color out of range");
         }
         return colour;
     }
 
-    public Object[] callMethod(ILuaContext context, int method, Object[] args)
-            throws Exception{
+    @Override
+    public String[] getMethodNames(){
+        return new String[]{
+                "write",
+                "scroll",
+                "setCursorPos",
+                "setCursorBlink",
+                "getCursorPos",
+                "getSize",
+                "clear",
+                "clearLine",
+                "setTextColor",
+                "setBackgroundColor"
+        };
+    }
+
+    @Override
+    public Object[] callMethod(ILuaContext context, int method, Object[] args) throws Exception{
         switch(method){
-            case 0:
+            case 0: //write
                 String text;
-                if((args.length > 0) && (args[0] != null))
+                if(args.length > 0 && args[0] != null){
                     text = args[0].toString();
-                else{
+                }else{
                     text = "";
                 }
 
@@ -65,8 +85,8 @@ public class TermApi implements ILuaAPI {
                     this.terminal.setCursorPos(this.terminal.getCursorX() + text.length(), this.terminal.getCursorY());
                 }
                 return null;
-            case 1:
-                if((args.length != 1) || (args[0] == null) || (!(args[0] instanceof Double))){
+            case 1: //scroll
+                if(args.length != 1 || args[0] == null || !(args[0] instanceof Double)){
                     throw new Exception("Expected number");
                 }
 
@@ -75,8 +95,8 @@ public class TermApi implements ILuaAPI {
                     this.terminal.scroll(y);
                 }
                 return null;
-            case 2:
-                if((args.length != 2) || (args[0] == null) || (!(args[0] instanceof Double)) || (args[1] == null) || (!(args[1] instanceof Double))){
+            case 2: //setCursorPos
+                if(args.length != 2 || args[0] == null || !(args[0] instanceof Double) || args[1] == null || !(args[1] instanceof Double)){
                     throw new Exception("Expected number, number");
                 }
 
@@ -86,49 +106,47 @@ public class TermApi implements ILuaAPI {
                     this.terminal.setCursorPos(x, y1);
                 }
                 return null;
-            case 3:
-                if((args.length != 1) || (args[0] == null) || (!(args[0] instanceof Boolean))){
+            case 3: //setCursorBlink
+                if(args.length != 1 || args[0] == null || !(args[0] instanceof Boolean)){
                     throw new Exception("Expected boolean");
                 }
-                boolean b = ((Boolean) args[0]).booleanValue();
+                boolean b = (Boolean) args[0];
                 synchronized(this.terminal){
                     this.terminal.setCursorBlink(b);
                 }
                 return null;
-            case 4:
+            case 4: //getCursorPos
                 synchronized(this.terminal){
                     x = this.terminal.getCursorX();
                     y = this.terminal.getCursorY();
                 }
-                return new Object[]{Integer.valueOf(x + 1), Integer.valueOf(y + 1)};
-            case 5:
+                return new Object[]{x + 1, y + 1};
+            case 5: //getSize
                 int width;
                 int height;
                 synchronized(this.terminal){
                     width = this.terminal.getWidth();
                     height = this.terminal.getHeight();
                 }
-                return new Object[]{Integer.valueOf(width), Integer.valueOf(height)};
-            case 6:
+                return new Object[]{width, height};
+            case 6: //clear
                 synchronized(this.terminal){
                     this.terminal.clear();
                 }
                 return null;
-            case 7:
+            case 7: //clearLine
                 synchronized(this.terminal){
                     this.terminal.clearLine();
                 }
                 return null;
-            case 8:
-            case 9:
-                int color = parseColour(args);
+            case 8: //setTextColor
+                int color = parseColor(args);
                 synchronized(this.terminal){
                     this.terminal.setTextColor(color);
                 }
                 return null;
-            case 10:
-            case 11:
-                int color1 = parseColour(args);
+            case 9: //setBackgroundColor
+                int color1 = parseColor(args);
                 synchronized(this.terminal){
                     this.terminal.setBackgroundColor(color1);
                 }
