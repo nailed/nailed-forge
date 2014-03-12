041205f8dab8c38aaab4d6504c3b9aa9a635f480
jk-5 <jeffreykog@me.com>
2014-03-12 08:00:08 +0100

    Fixed tests for the json parser. More to come

--- Updater/src/test/java/jk_5/nailed/updater/JsonTest.java
+++ Updater/src/test/java/jk_5/nailed/updater/JsonTest.java
@@ -2,6 +2,7 @@ package jk_5.nailed.updater;
 
 import com.google.gson.Gson;
 import jk_5.nailed.updater.json.Library;
+import jk_5.nailed.updater.json.LibraryList;
 import jk_5.nailed.updater.json.RestartLevel;
 import jk_5.nailed.updater.json.serialization.LibraryListSerializer;
 import org.junit.Assert;
@@ -24,23 +25,23 @@ public class JsonTest {
 
     @Test
     public void testEnumSerialization(){
-        Assert.assertEquals("\"game\"", gson.toJson(RestartLevel.GAME));
-        Assert.assertEquals("\"launcher\"", gson.toJson(RestartLevel.LAUNCHER));
-        Assert.assertEquals("\"nothing\"", gson.toJson(RestartLevel.NOTHING));
+        for(RestartLevel level : RestartLevel.values()){
+            Assert.assertEquals("\"" + level.name().toLowerCase() + "\"", gson.toJson(level));
+        }
     }
 
     @Test
     public void testEnumDeserialization(){
-        Assert.assertEquals(RestartLevel.GAME, gson.fromJson("\"game\"", RestartLevel.class));
-        Assert.assertEquals(RestartLevel.LAUNCHER, gson.fromJson("\"launcher\"", RestartLevel.class));
-        Assert.assertEquals(RestartLevel.NOTHING, gson.fromJson("\"nothing\"", RestartLevel.class));
+        for(RestartLevel level : RestartLevel.values()){
+            Assert.assertEquals(level, gson.fromJson("\"" + level.name().toLowerCase() + "\"", RestartLevel.class));
+        }
     }
 
     @Test
     public void testEnumDeserializationAcceptsUppercase(){
-        Assert.assertEquals(RestartLevel.GAME, gson.fromJson("\"GaMe\"", RestartLevel.class));
-        Assert.assertEquals(RestartLevel.LAUNCHER, gson.fromJson("\"lAunChEr\"", RestartLevel.class));
-        Assert.assertEquals(RestartLevel.NOTHING, gson.fromJson("\"NOThinG\"", RestartLevel.class));
+        for(RestartLevel level : RestartLevel.values()){
+            Assert.assertEquals(level, gson.fromJson("\"" + TestUtils.shuffleCase(level.name().toLowerCase()) + "\"", RestartLevel.class));
+        }
     }
 
     @Test
@@ -70,11 +71,126 @@ public class JsonTest {
                 "  \"restart\": \"nothing\"\n" +
                 "}", Library.class);
 
-        Assert.assertNotNull(lib);
-        Assert.assertEquals(0, lib.rev);
-        Assert.assertEquals("dest", lib.destination);
-        Assert.assertEquals("location", lib.location);
-        Assert.assertEquals(RestartLevel.NOTHING, lib.restart);
-        Assert.assertNull(lib.name);
+        Assert.assertNotNull("Output should not be null", lib);
+        Assert.assertEquals("Rev should equal 0", 0, lib.rev);
+        Assert.assertEquals("Destination should equal \"dest\"", "dest", lib.destination);
+        Assert.assertEquals("Location should equal \"location\"", "location", lib.location);
+        Assert.assertEquals("RestartLevel should equal RestartLevel.NOTHING", RestartLevel.NOTHING, lib.restart);
+        Assert.assertNull("Name should not be touched", lib.name);
+    }
+
+    @Test
+    public void testLibraryListSerialization(){
+        LibraryList list = new LibraryList();
+        Library lib = new Library();
+        lib.rev = 0;
+        lib.restart = RestartLevel.NOTHING;
+        lib.name = "name";
+        lib.location = "location";
+        lib.destination = "dest";
+
+        list.libraries.add(lib);
+
+        Assert.assertEquals(
+                "{\n" +
+                        "  \"name\": {\n" +
+                        "    \"rev\": 0,\n" +
+                        "    \"destination\": \"dest\",\n" +
+                        "    \"location\": \"location\",\n" +
+                        "    \"restart\": \"nothing\"\n" +
+                        "  }\n" +
+                        "}", this.gson.toJson(list));
+    }
+
+    @Test
+    public void testLibraryListSerializationMultiple(){
+        LibraryList list = new LibraryList();
+        Library lib = new Library();
+        lib.rev = 0;
+        lib.restart = RestartLevel.NOTHING;
+        lib.name = "name";
+        lib.location = "location";
+        lib.destination = "dest";
+        list.libraries.add(lib);
+
+        lib = new Library();
+        lib.rev = 1;
+        lib.restart = RestartLevel.GAME;
+        lib.name = "name2";
+        lib.location = "location2";
+        lib.destination = "dest2";
+        list.libraries.add(lib);
+
+        Assert.assertEquals(
+                "{\n" +
+                        "  \"name\": {\n" +
+                        "    \"rev\": 0,\n" +
+                        "    \"destination\": \"dest\",\n" +
+                        "    \"location\": \"location\",\n" +
+                        "    \"restart\": \"nothing\"\n" +
+                        "  },\n" +
+                        "  \"name2\": {\n" +
+                        "    \"rev\": 1,\n" +
+                        "    \"destination\": \"dest2\",\n" +
+                        "    \"location\": \"location2\",\n" +
+                        "    \"restart\": \"game\"\n" +
+                        "  }\n" +
+                        "}", this.gson.toJson(list));
+    }
+
+    @Test
+    public void testLibraryListDeserialization(){
+        LibraryList list = this.gson.fromJson("{\n" +
+                "  \"name\": {\n" +
+                "    \"rev\": 0,\n" +
+                "    \"destination\": \"dest\",\n" +
+                "    \"location\": \"location\",\n" +
+                "    \"restart\": \"nothing\"\n" +
+                "  }\n" +
+                "}", LibraryList.class);
+
+        Assert.assertNotNull("Output should not be null", list);
+        Assert.assertNotNull("LibraryList content should not be null", list.libraries);
+        Assert.assertEquals("LibraryList should have 1 entry", 1, list.libraries.size());
+        Library lib = list.libraries.get(0);
+        Assert.assertEquals("Rev should equal 0", 0, lib.rev);
+        Assert.assertEquals("Destination should equal \"dest\"", "dest", lib.destination);
+        Assert.assertEquals("Location should equal \"location\"", "location", lib.location);
+        Assert.assertEquals("RestartLevel should equal RestartLevel.NOTHING", RestartLevel.NOTHING, lib.restart);
+        Assert.assertEquals("Name should equal \"name\"", "name", lib.name);
+    }
+
+    @Test
+    public void testLibraryListDeserializationMultiple(){
+        LibraryList list = this.gson.fromJson("{\n" +
+                "  \"name\": {\n" +
+                "    \"rev\": 0,\n" +
+                "    \"destination\": \"dest\",\n" +
+                "    \"location\": \"location\",\n" +
+                "    \"restart\": \"nothing\"\n" +
+                "  },\n" +
+                "  \"name2\": {\n" +
+                "    \"rev\": 1,\n" +
+                "    \"destination\": \"dest2\",\n" +
+                "    \"location\": \"location2\",\n" +
+                "    \"restart\": \"game\"\n" +
+                "  }\n" +
+                "}", LibraryList.class);
+
+        Assert.assertNotNull("Output should not be null", list);
+        Assert.assertNotNull("LibraryList content should not be null", list.libraries);
+        Assert.assertEquals("LibraryList should have 2 entries", 2, list.libraries.size());
+        Library lib = list.libraries.get(0);
+        Assert.assertEquals("Rev should equal 0", 0, lib.rev);
+        Assert.assertEquals("Destination should equal \"dest\"", "dest", lib.destination);
+        Assert.assertEquals("Location should equal \"location\"", "location", lib.location);
+        Assert.assertEquals("RestartLevel should equal RestartLevel.NOTHING", RestartLevel.NOTHING, lib.restart);
+        Assert.assertEquals("Name should equal \"name\"", "name", lib.name);
+        lib = list.libraries.get(1);
+        Assert.assertEquals("Rev should equal 1", 1, lib.rev);
+        Assert.assertEquals("Destination should equal \"dest2\"", "dest2", lib.destination);
+        Assert.assertEquals("Location should equal \"location2\"", "location2", lib.location);
+        Assert.assertEquals("RestartLevel should equal RestartLevel.GAME", RestartLevel.GAME, lib.restart);
+        Assert.assertEquals("Name should equal \"name2\"", "name2", lib.name);
     }
 }
