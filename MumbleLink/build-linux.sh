f0a10a7adff4810192134249b15b2f26af4cfff8
jk-5 <jeffreykog@me.com>
2014-03-24 07:40:24 +0100

    Added linux build scripts

---
+++ MumbleLink/build-linux.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+JAVAPATH=/usr/lib/jvm/java-6-sun-1.6.0.24/include
+
+# 32 bit
+rm -R src/main/resources/assets/nailedmumble/native/linux32
+mkdir src/main/resources/assets/nailedmumble/native/linux32
+
+echo "Compiling linux32"
+
+gcc -o src/main/resources/assets/nailedmumble/native/linux32/libMumbleLink.so -shared \
+     -Wl,-soname,src/main/resources/assets/nailedmumble/native/linux32/libMumbleLink.so  \
+     -I${JAVAPATH}/ \
+     -I${JAVAPATH}/linux \
+     -lrt \
+     src/main/c/mumble.c
+
+# 64 bit
+rm -R src/main/resources/assets/nailedmumble/native/linux64
+mkdir src/main/resources/assets/nailedmumble/native/linux64
+
+echo "Compiling linux64"
+
+gcc -o src/main/resources/assets/nailedmumble/native/linux64/libMumbleLink.so -m64 -shared -fPIC \
+     -Wl,-soname,src/main/resources/assets/nailedmumble/native/linux64/libMumbleLink.so  \
+     -I${JAVAPATH}/ \
+     -I${JAVAPATH}/linux \
+     -lrt \
+     src/main/c/mumble/c \
+       -lstdc++ \
+       -m64 \
+       -I/usr/include/c++/4.4/i686-linux-gnu
