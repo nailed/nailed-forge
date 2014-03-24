474ad86d19e7724a61d35266461861a47b3c3076
Jeffrey Kog <jeffreykog@me.com>
2014-03-24 02:54:48 -0400

    Compiled 64 bit linux natives

--- MumbleLink/build-linux.sh
+++ MumbleLink/build-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-JAVAPATH=/usr/lib/jvm/java-6-sun-1.6.0.24/include
+JAVAPATH=/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.51/include
 
 # 32 bit
 rm -R src/main/resources/assets/nailedmumble/native/linux32
@@ -8,7 +8,7 @@ mkdir src/main/resources/assets/nailedmumble/native/linux32
 
 echo "Compiling linux32"
 
-gcc -o src/main/resources/assets/nailedmumble/native/linux32/libMumbleLink.so -shared \
+gcc -o src/main/resources/assets/nailedmumble/native/linux32/libMumbleLink.so -m32 -shared -fPIC \
      -Wl,-soname,src/main/resources/assets/nailedmumble/native/linux32/libMumbleLink.so  \
      -I${JAVAPATH}/ \
      -I${JAVAPATH}/linux \
@@ -26,6 +26,4 @@ gcc -o src/main/resources/assets/nailedmumble/native/linux64/libMumbleLink.so -m
      -I${JAVAPATH}/ \
      -I${JAVAPATH}/linux \
      -lrt \
-     src/main/c/mumble.c \
-       -m64 \
-       -I/usr/include/c++/4.4/i686-linux-gnu
+     src/main/c/mumble.c
