3bf9fdecdbb62612649a80224530f31f8fee85c4
jk-5 <jeffreykog@me.com>
2014-04-07 21:18:56 +0200

    Add permission node 'minecraft.chat'. This is required to be able to talk ingame

--- Server/src/main/java/jk_5/nailed/permissions/PermissionEventHandler.java
+++ Server/src/main/java/jk_5/nailed/permissions/PermissionEventHandler.java
@@ -1,12 +1,17 @@
 package jk_5.nailed.permissions;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import jk_5.nailed.api.NailedAPI;
+import jk_5.nailed.api.player.Player;
 import jk_5.nailed.server.command.PermissionCommand;
 import net.minecraft.command.server.CommandBlockLogic;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.IChatComponent;
 import net.minecraftforge.event.CommandEvent;
+import net.minecraftforge.event.ServerChatEvent;
 import net.minecraftforge.permissions.api.PermissionsManager;
 
 /**
@@ -16,6 +21,8 @@ import net.minecraftforge.permissions.api.PermissionsManager;
  */
 @SuppressWarnings("unused")
 public class PermissionEventHandler {
+
+    public static final String CHATNODE = "minecraft.chat";
 
     @SubscribeEvent
     public void onCommand(CommandEvent event){
@@ -40,6 +47,22 @@ public class PermissionEventHandler {
                     event.sender.addChatMessage(message);
                 }
             }
+        }
+    }
+
+    @SubscribeEvent
+    public void onChat(ServerChatEvent event){
+        Player player = NailedAPI.getPlayerRegistry().getPlayer(event.player);
+        if(player == null){
+            event.setCanceled(true);
+            IChatComponent message = new ChatComponentText("Your message has been cancelled because you don\'t exist on the server. Relog to fix this problem!");
+            message.getChatStyle().setColor(EnumChatFormatting.RED);
+            event.player.addChatMessage(message);
+        }else if(!PermissionsManager.checkPerm(player.getEntity(), CHATNODE)){
+            event.setCanceled(true);
+            IChatComponent message = new ChatComponentText("You don\'t have the permission to talk");
+            message.getChatStyle().setColor(EnumChatFormatting.RED);
+            event.player.addChatMessage(message);
         }
     }
 }
