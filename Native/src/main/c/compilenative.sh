4a92fb51d47fa3c3162176fc58d35a6d396b4d11
jk-5 <jeffreykog@me.com>
2014-05-11 10:50:07 +0200

    Remove old and unused code. Will revert it when i get it working again

--- Native/src/main/c/compilenative.sh
+++
@@ -1,1 +0,0 @@
-gcc -I"/Library/Java/JavaVirtualMachines/jdk1.7.0_40.jdk/Contents/Home/include" -I"/Library/Java/JavaVirtualMachines/jdk1.7.0_40.jdk/Contents/Home/include/darwin" -shared -o libnativecipher.jnilib NativeCipherImpl.c
