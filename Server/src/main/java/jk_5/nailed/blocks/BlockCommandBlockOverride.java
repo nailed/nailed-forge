04ca96f212c24f2aea88bef173a3eea2d50a12cf
jk-5 <jeffreykog@me.com>
2014-05-12 18:40:36 +0200

    Initial commit on commandblock overriding and removed supereditmode

---
+++ Server/src/main/java/jk_5/nailed/blocks/BlockCommandBlockOverride.java
@@ -0,0 +1,21 @@
+package jk_5.nailed.blocks;
+
+import jk_5.nailed.NailedLog;
+import net.minecraft.block.BlockCommandBlock;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityCommandBlock;
+import net.minecraft.world.World;
+
+/**
+ * No description given
+ *
+ * @author jk-5
+ */
+public class BlockCommandBlockOverride extends BlockCommandBlock {
+
+    @Override
+    public TileEntity createNewTileEntity(World world, int meta) {
+        NailedLog.info("Oh hey! We have overridden the commandblock");
+        return new TileEntityCommandBlock();
+    }
+}
--- Server/src/main/java/jk_5/nailed/util/Utils.java
+++ Server/src/main/java/jk_5/nailed/util/Utils.java
@@ -16,69 +16,6 @@ public class Utils {
 
     private static final Pattern formattingCodePattern = Pattern.compile("(?i)" + String.valueOf('\u00a7') + "[0-9A-FK-OR]");
 
-    public static String secondsToShortTimeString(long secs){
-        long hours = secs / 3600;
-        long minutes = (secs % 3600) / 60;
-        long seconds = secs % 60;
-        StringBuilder builder = new StringBuilder();
-        boolean append = false;
-        if(hours != 0 || append){
-            builder.append(hours);
-            builder.append(":");
-            append = true;
-        }
-        if(minutes != 0 || append){
-            builder.append(minutes);
-            builder.append(":");
-            append = true;
-        }
-        if(seconds != 0 || append){
-            if(seconds < 10 && append) builder.append("0");
-            builder.append(seconds);
-            if(!append){
-                builder.append(ChatColor.RESET);
-                builder.append(" Second");
-                if(seconds != 1) builder.append("s");
-            }
-        }
-        if(seconds == 0 && !append) builder.append("0 Seconds");
-        return builder.toString();
-    }
-
-    public static String secondsToLongTimeString(long secs){
-        if (secs < 60) return String.format("%d second%s", secs, (secs == 1) ? "" : "s");
-        long hours = secs / 3600;
-        long minutes = (secs % 3600) / 60;
-        long seconds = secs % 60;
-        StringBuilder builder = new StringBuilder();
-        boolean hasText = false;
-        if(hours > 0){
-            builder.append(hours);
-            builder.append(" hour");
-            if(hours > 1) builder.append("s");
-            hasText = true;
-        }
-        if(minutes > 0){
-            if (hasText){
-                if(secs > 0) builder.append(", ");
-                else builder.append(" and ");
-            }
-            builder.append(minutes);
-            builder.append(" minute");
-            if(minutes > 1) builder.append("s");
-            hasText = true;
-        }
-        if(seconds > 0){
-            if (hasText) builder.append(" and ");
-            builder.append(seconds);
-            builder.append(" second");
-            if(seconds > 1) builder.append("s");
-            hasText = true;
-        }
-        if(!hasText) builder.append("0 seconds");
-        return builder.toString();
-    }
-
     public static String formatColors(String message){
         char[] b = message.toCharArray();
         for(int i = 0; i < b.length - 1; i++){
@@ -88,21 +25,6 @@ public class Utils {
             }
         }
         return new String(b);
-    }
-
-    public static String replaceAllIgnoreCase(String text, String search, String replacement){
-        if(search.equals(replacement)){
-            return text;
-        }
-        StringBuilder buffer = new StringBuilder(text);
-        String lowerSearch = search.toLowerCase();
-        int i;
-        int prev = 0;
-        while((i = buffer.toString().toLowerCase().indexOf(lowerSearch, prev)) > -1){
-            buffer.replace(i, i + search.length(), replacement);
-            prev = i + replacement.length();
-        }
-        return buffer.toString();
     }
 
     public static String parseTime(int seconds){
@@ -144,13 +66,6 @@ public class Utils {
         builder.append(" second");
         if(seconds != 1) builder.append("s");
         return builder.toString().trim();
-    }
-
-    public static String leadingZero(int number){
-        StringBuilder builder = new StringBuilder();
-        if(number > 10) builder.append("0");
-        builder.append(number);
-        return builder.toString();
     }
 
     @SuppressWarnings("unchecked")
@@ -196,6 +111,7 @@ public class Utils {
             IChatComponent comp = it.next();
             if(comp.getUnformattedTextForChat().isEmpty()){
                 it.remove();
+                continue;
             }
             minifyChatComponent(comp);
         }
