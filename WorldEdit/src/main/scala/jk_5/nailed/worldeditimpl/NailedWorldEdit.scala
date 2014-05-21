016bc8da8b50bae10eab9b8f4698bac70c858de9
jk-5 <jeffreykog@me.com>
2014-05-21 13:03:22 +0200

    Clients don't have to have worldedit installed

--- WorldEdit/src/main/scala/jk_5/nailed/worldeditimpl/NailedWorldEdit.scala
+++ WorldEdit/src/main/scala/jk_5/nailed/worldeditimpl/NailedWorldEdit.scala
@@ -3,6 +3,8 @@ package jk_5.nailed.worldeditimpl
 import cpw.mods.fml.common.Mod
 import cpw.mods.fml.common.Mod.EventHandler
 import cpw.mods.fml.common.event.FMLPreInitializationEvent
+import cpw.mods.fml.common.network.NetworkCheckHandler
+import cpw.mods.fml.relauncher.Side
 
 /**
  * No description given
@@ -11,6 +13,8 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent
  */
 @Mod(modid = "NailedWorldEdit", name = "Nailed WorldEdit", version = "0.1-SNAPSHOT", modLanguage = "scala")
 object NailedWorldEdit {
+
+  @NetworkCheckHandler def accept(versions: java.util.Map[String, String], side: Side) = true
 
   @EventHandler def preInit(event: FMLPreInitializationEvent){
     //WorldEdit.
