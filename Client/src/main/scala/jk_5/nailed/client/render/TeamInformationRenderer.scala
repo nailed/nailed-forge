8a9911e3a040e7951c8768c1e44ec88f8b0c6c50
jk-5 <jeffreykog@me.com>
2014-05-30 21:14:05 +0200

    Fixed a startup crash on the client

--- Client/src/main/scala/jk_5/nailed/client/render/TeamInformationRenderer.scala
+++ Client/src/main/scala/jk_5/nailed/client/render/TeamInformationRenderer.scala
@@ -10,7 +10,6 @@ import org.lwjgl.opengl.GL11
 import scala.collection.mutable
 import net.minecraft.client.entity.AbstractClientPlayer
 import net.minecraft.client.renderer.Tessellator
-import net.minecraft.client.Minecraft
 import scala.util.Random
 
 /**
@@ -18,7 +17,7 @@ import scala.util.Random
  *
  * @author jk-5
  */
-object TeamInformationRenderer extends Gui {
+object TeamInformationRenderer {
   val random = new Random()
   val left = List(getRenderer("jk_5"), getRenderer("Dabadooba"), getRenderer("ikzelf1248"))
   val topRight = List(getRenderer("Dinnerbone"), getRenderer("Grumm"), getRenderer("Jeb_"))
